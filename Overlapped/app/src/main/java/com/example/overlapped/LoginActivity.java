f608cdc7ad182375352486ec8e3737d533681ee3
virkSehbazz12 <sehbazz@ualberta.ca>
2023-01-07 14:47:19 -0700

    created login activity_login

--- Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
+++ Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
@@ -7,14 +7,16 @@ import android.os.Bundle;
 public class LoginActivity extends AppCompatActivity {
 
     Database db;
+
+    GoogleSignInOptions googleSignInOptions;
+    GoogleSignInClient googleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        db = Database.getInstance();
-
-
+        googleSing
 
     }
 }
