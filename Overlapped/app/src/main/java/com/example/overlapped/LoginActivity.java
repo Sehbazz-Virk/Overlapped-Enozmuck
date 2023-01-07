e7d03f606413220584393b5b743c8e0ba0420938
Gurbir Sandha <77547332+gurbirsandha@users.noreply.github.com>
2023-01-07 12:20:33 -0700

    firestore setup almost done

--- Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
+++ Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Database db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        db = Database.getInstance();
     }
 }
