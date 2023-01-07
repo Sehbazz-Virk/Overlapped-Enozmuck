0a315e454b80777cc3c7d74c8f87283db6a10b19
Gurbir Sandha <77547332+gurbirsandha@users.noreply.github.com>
2023-01-07 13:12:36 -0700

    what the hell

--- Overlapped/app/src/main/java/com/example/overlapped/Database.java
+++ Overlapped/app/src/main/java/com/example/overlapped/Database.java
@@ -1,7 +1,18 @@
 package com.example.overlapped;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class Database {
     private static Database db_ = null;
@@ -11,7 +22,7 @@ public class Database {
 
     private Database() {
         db = FirebaseFirestore.getInstance();
-        events = db.collection("events");
+
     }
 
     public static Database getInstance() {
@@ -19,5 +30,22 @@ public class Database {
             db_ = new Database();
         }
         return db_;
+    }
+
+    public void addEventTest(UUID ownerID) {
+        events = db.collection("eventsTester");
+        Map<String, Object> eventDetails = new HashMap<>();
+        eventDetails.put("owner", ownerID.toString());
+        events.document(UUID.randomUUID().toString()).set(eventDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d("wow", "yay");
+                } else {
+                    Log.d("balls","balls");
+                }
+            }
+        });
+        //events.add(eventDetails);
     }
 }
--- Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
+++ Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.UUID;
+
 public class LoginActivity extends AppCompatActivity {
 
     Database db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         db = Database.getInstance();
+        db.addEventTest(UUID.randomUUID());
 
 
 
