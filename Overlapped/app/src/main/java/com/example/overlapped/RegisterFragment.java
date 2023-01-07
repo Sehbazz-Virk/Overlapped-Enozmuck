5f4ffbc6f7f4b5ca5dc086f7aaaf89909b48626a
virkSehbazz12 <sehbazz@ualberta.ca>
2023-01-07 16:47:03 -0700

    gabagool again

--- Overlapped/app/src/main/java/com/example/overlapped/RegisterFragment.java
+++ Overlapped/app/src/main/java/com/example/overlapped/RegisterFragment.java
@@ -1,6 +1,7 @@
 package com.example.overlapped;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,12 +10,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterFragment extends Fragment {
     Button confirm;
     Button cancel;
     EditText emailEntry;
     EditText passwordEntry;
     View myView;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     public RegisterFragment() {
         super(R.layout.register_fragment);
@@ -33,6 +41,34 @@ public class RegisterFragment extends Fragment {
         this.cancel = (Button) this.myView.findViewById(R.id.button_cancel_register);
         this.emailEntry = (EditText) this.myView.findViewById(R.id.register_email_entry);
         this.passwordEntry = (EditText) this.myView.findViewById(R.id.register_password_entry);
+    }
+
+    private void signUpEmailPassword() {
+
+        String email = emailEntry.getText().toString();
+        String password = passwordEntry.getText().toString();
+
+        if (email.equals("") || password.equals("")) {
+            passwordEntry.setError("Invalid signup");
+            emailEntry.setError("Invalid signup");
+            return;
+        }
+        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Log.i("AUTH", "SIGN UP SUCCEEDED");
+                passwordEntry.setError(null);
+                emailEntry.setError(null);
+                ((LoginActivity)getActivity()).launchApp();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("AUTH", "SIGN UP FAILED");
+                passwordEntry.setError("Invalid signup");
+                emailEntry.setError("Invalid signup");
+            }
+        });
     }
 
 }
