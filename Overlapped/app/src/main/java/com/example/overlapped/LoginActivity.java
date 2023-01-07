e7a4d9f187ab6e9f92f9cb8bb7e656d556d6ed99
virkSehbazz12 <sehbazz@ualberta.ca>
2023-01-07 16:26:56 -0700

    added sign in method.

--- Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
+++ Overlapped/app/src/main/java/com/example/overlapped/LoginActivity.java
@@ -1,18 +1,50 @@
 package com.example.overlapped;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Database db;
+    private Database db;
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        db = Database.getInstance();
+        if (auth.getCurrentUser() != null) {
+            launchApp();
+        } else {
+            launchLogin();
+        }
+
+
 
     }
+
+    public void launchLogin() {
+
+        // launch login fragment
+
+    }
+
+    public void launchApp() {
+
+        Intent intent = new Intent(this, NavigationActivity.class);
+        startActivity(intent);
+
+    }
+
 }
--- Overlapped/app/src/main/java/com/example/overlapped/LoginFragment.java
+++ Overlapped/app/src/main/java/com/example/overlapped/LoginFragment.java
@@ -1,12 +1,62 @@
 package com.example.overlapped;
 
 
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class LoginFragment extends Fragment {
 
+    EditText email_ET, password_ET;
+    Button confirm_BT;
+    Button signup_BT;
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
     public LoginFragment() {
-        super()
+        super();
     }
+
+    /**
+     * This method signs in a user. If successfully signed in, the app opens. Otherwise, the user is notified to re-attempt login
+     * @param email email address specified by user
+     * @param password password entered by user
+     */
+    private void signInEmailPassword(String email, String password) {
+        if (email.equals("") || password.equals("")) {
+            password_ET.setError("Invalid login");
+            email_ET.setError("Invalid login");
+            return;
+        }
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Log.i("AUTH", "LOGGED IN");
+                password_ET.setError(null);
+                email_ET.setError(null);
+                ((LoginActivity)getActivity()).launchApp();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("AUTH", "LOG IN FAILED");
+                password_ET.setError("Invalid login");
+                email_ET.setError("Invalid login");
+
+            }
+        });
+
+    }
+
+
 
 }
