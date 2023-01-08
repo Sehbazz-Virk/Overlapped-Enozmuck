2ab1a4cef6695a95041f5ac788b1dd960189b213
Jason Sommerville <nosajsom@gmail.com>
2023-01-07 23:59:33 -0700

    Smart Merge

--- Overlapped/app/src/main/java/com/example/overlapped/PotentialEvent.java
+++ Overlapped/app/src/main/java/com/example/overlapped/PotentialEvent.java
@@ -3,6 +3,8 @@ package com.example.overlapped;
 import android.util.Pair;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -22,5 +24,22 @@ public abstract class PotentialEvent extends Event {
 
     }
 
+    public void findTime() {
+
+     Integer bestScore = -1;
+     LocalDateTime bestTime;
+
+
+     // sort the 4-element array of "tuples" by the first element
+     Arrays.sort(array, new Comparator<Object[]>() {
+        @Override
+        public int compare(Object[] o1, Object[] o2) {
+            Integer i1 = (Integer) (o1[0]);
+            Integer i2 = (Integer) (o2[0]);
+            return i1.compareTo(i2);
+        }
+     });
+
+    }
 
 }
